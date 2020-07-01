ab0c3a24d0a0317cb4d2c09387434042f1337376
Chris Patrick <atomize101@gmail.com>
2020-07-01 09:20:39 -0400

    Hooking up event listeners

--- scripts.js
+++ scripts.js
@@ -26,5 +26,15 @@ function togglePlay() {
 }
 */
 
+function updateButton() {
+    const icon = this.paused ? '►' : '❚ ❚';
+    toggle.textContent = icon;
+}
 
 // Hook up event listeners
+
+video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+
+toggle.addEventListener('click', togglePlay);
